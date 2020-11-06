1d1515b115e3e1bc918b4db1f9b8807e6e881334
baerdata <baerdata.ger@googlemail.com>
2020-11-06 01:21:42 +0100

    add timestamp and process-image hook

--- src/app.hooks.ts
+++ src/app.hooks.ts
@@ -1,13 +1,17 @@
+import {setTimestamp } from './hooks/setTimestamp'
+
 // Application hooks that run for every service
 // Don't remove this comment. It's needed to format import lines nicely.
+
+
 
 export default {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
+    create: [setTimestamp('createdAt')],
+    update: [setTimestamp('updatedAt')],
     patch: [],
     remove: []
   },
